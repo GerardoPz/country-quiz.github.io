9ce72b149a365cbedf7f1c0e07faf299a9bf1c2a
Gerardo <pliegolugohectorgerardo@gmail.com>
2023-11-20 18:47:16 -0600

    Cambio de preguntas

--- script.js
+++ script.js
@@ -1,96 +1,111 @@
 fetch("https://restcountries.com/v3.1/all")
   .then((response) => response.json())
   .then((data) => {
-    // Selecciona un país al azar de la lista
-    const randomCountry = data[Math.floor(Math.random() * data.length)];
+    // Function to generate a new question
+    const generateQuestion = () => {
+      // Selecciona un país al azar de la lista
+      const randomCountry = data[Math.floor(Math.random() * data.length)];
 
-    let question = document.getElementById("question");
-    // Genera la pregunta y la respuesta
-    const questionGenerate = `¿Which country is ${randomCountry.name.common} the capital?`;
+      let question = document.getElementById("question");
+      // Genera la pregunta y la respuesta
+      const questionGenerate = `¿Which country is ${randomCountry.name.common} the capital?`;
 
-    // Genera 4 respuestas con solo una correcta
-    const answers = [];
-    const correctAnswerIndex = Math.floor(Math.random() * 4);
-    for (let i = 0; i < 4; i++) {
-      let answer;
-      if (i === correctAnswerIndex) {
-        answer = randomCountry.capital[0];
-      } else {
-        let randomCountryIndex = Math.floor(Math.random() * data.length);
-        while (randomCountryIndex === correctAnswerIndex) {
-          randomCountryIndex = Math.floor(Math.random() * data.length);
-        }
-        answer = data[randomCountryIndex].capital[0];
-      }
-      answers.push(answer);
-    }
-
-    console.log(questionGenerate);
-    question.innerHTML = questionGenerate;
-    const answersContainer = document.getElementById("options-answers");
-    console.log(`Respuestas:`);
-    answers.forEach((answer, index) => {
-      console.log(`${index + 1}. ${answer}`);
-      const answerElement = document.createElement("div");
-
-      // Asigna las clases y el id correspondiente a cada respuesta
-      answerElement.className = `answer`;
-      answerElement.id = `${index + 1}`;
-
-      // Asigna el texto de la respuesta al elemento
-      answerElement.textContent = answer;
-
-      // Agrega el elemento al contenedor de respuestas
-      answersContainer.appendChild(answerElement);
-
-      // Agrega el evento de escucha al clic en la respuesta
-      answerElement.addEventListener("click", () => {
-        // Realiza la acción deseada cuando el usuario seleccione una respuesta
-        if (answer === answers[correctAnswerIndex]) {
-          console.log("Respuesta correcta");
-          // Agrega el div de respuesta
-          const answerDiv = document.createElement("div");
-          answerDiv.className = "answer-div";
-
-          // Agrega el texto de la respuesta al div
-          const answerText = document.createElement("span");
-          answerText.textContent = answer;
-          answerDiv.appendChild(answerText);
-
-          // Agrega el SVG de respuesta correcta al div
-          const svgCorrect = document.createElement("img");
-          svgCorrect.src = "./src/Check_round_fill.svg";
-          svgCorrect.className = "correct-svg";
-          answerDiv.appendChild(svgCorrect);
-
-          // Reemplaza el contenido del elemento respuesta con el div
-          answerElement.innerHTML = "";
-          answerElement.appendChild(answerDiv);
+      // Genera 4 respuestas con solo una correcta
+      const answers = [];
+      const correctAnswerIndex = Math.floor(Math.random() * 4);
+      for (let i = 0; i < 4; i++) {
+        let answer;
+        if (i === correctAnswerIndex) {
+          answer = randomCountry.capital[0];
         } else {
-          console.log("Respuesta incorrecta");
-          // Agrega el div de respuesta
-          const answerDiv = document.createElement("div");
-          answerDiv.className = "answer-div";
-
-          // Agrega el texto de la respuesta al div
-          const answerText = document.createElement("span");
-          answerText.textContent = answer;
-          answerDiv.appendChild(answerText);
-
-          // Agrega el SVG de respuesta correcta al div
-          const svgCorrect = document.createElement("img");
-          svgCorrect.src = "./src/Close_round_fill.svg";
-          svgCorrect.className = "correct-svg";
-          answerDiv.appendChild(svgCorrect);
-
-          // Reemplaza el contenido del elemento respuesta con el div
-          answerElement.innerHTML = "";
-          answerElement.appendChild(answerDiv);
+          let randomCountryIndex = Math.floor(Math.random() * data.length);
+          while (randomCountryIndex === correctAnswerIndex) {
+            randomCountryIndex = Math.floor(Math.random() * data.length);
+          }
+          answer = data[randomCountryIndex].capital[0];
         }
-        console.log(`El usuario seleccionó la respuesta ${answer}`);
+        answers.push(answer);
+      }
+
+      console.log(questionGenerate);
+      question.innerHTML = questionGenerate;
+      const answersContainer = document.getElementById("options-answers");
+      console.log(`Respuestas:`);
+      answers.forEach((answer, index) => {
+        console.log(`${index + 1}. ${answer}`);
+        const answerElement = document.createElement("div");
+
+        // Asigna las clases y el id correspondiente a cada respuesta
+        answerElement.className = `answer`;
+        answerElement.id = `${index + 1}`;
+
+        // Asigna el texto de la respuesta al elemento
+        answerElement.textContent = answer;
+
+        // Agrega el elemento al contenedor de respuestas
+        answersContainer.appendChild(answerElement);
+
+        // Agrega el evento de escucha al clic en la respuesta
+        answerElement.addEventListener("click", () => {
+          // Realiza la acción deseada cuando el usuario seleccione una respuesta
+          if (answer === answers[correctAnswerIndex]) {
+            console.log("Respuesta correcta");
+            // Agrega el div de respuesta
+            const answerDiv = document.createElement("div");
+            answerDiv.className = "answer-div";
+
+            answerElement.style.background =
+              "linear-gradient(#e65895, #bc6be8)";
+
+            // Agrega el texto de la respuesta al div
+            const answerText = document.createElement("span");
+            answerText.textContent = answer;
+            answerDiv.appendChild(answerText);
+
+            // Agrega el SVG de respuesta correcta al div
+            const svgCorrect = document.createElement("img");
+            svgCorrect.src = "./src/Check_round_fill.svg";
+            svgCorrect.className = "correct-svg";
+            answerDiv.appendChild(svgCorrect);
+
+            // Reemplaza el contenido del elemento respuesta con el div
+            answerElement.innerHTML = "";
+            answerElement.appendChild(answerDiv);
+          } else {
+            console.log("Respuesta incorrecta");
+            // Agrega el div de respuesta
+            const answerDiv = document.createElement("div");
+            answerDiv.className = "answer-div";
+
+            // Agrega el texto de la respuesta al div
+            const answerText = document.createElement("span");
+            answerText.textContent = answer;
+            answerDiv.appendChild(answerText);
+
+            // Agrega el SVG de respuesta correcta al div
+            const svgCorrect = document.createElement("img");
+            svgCorrect.src = "./src/Close_round_fill.svg";
+            svgCorrect.className = "correct-svg";
+            answerDiv.appendChild(svgCorrect);
+
+            // Reemplaza el contenido del elemento respuesta con el div
+            answerElement.innerHTML = "";
+            answerElement.appendChild(answerDiv);
+          }
+          console.log(`El usuario seleccionó la respuesta ${answer}`);
+
+          // Generate a new question after answering
+          setTimeout(() => {
+            answersContainer.innerHTML = "";
+            generateQuestion();
+          }, 1000);
+        });
       });
-    });
-    console.log(`La respuesta correcta es: ${answers[correctAnswerIndex]}`);
+      console.log(`La respuesta correcta es: ${answers[correctAnswerIndex]}`);
+    };
+
+    // Generate the first question
+    generateQuestion();
   })
   .catch((error) => {
     console.log(error);
